91fc35a6bad100bf23bd749ff77958e17e1a4969
nesrine ncibi <nesrine.ncibi@polytechnicien.tn>
2024-01-03 00:20:12 +0100

    scrap2

--- scrap.py
+++ scrap.py
@@ -7,7 +7,7 @@ import requests
 
 
 
-SECURE_URL='https://core.jax-delivery.com/api/clients?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NvcmUuamF4LWRlbGl2ZXJ5LmNvbS9hcGkvYXV0aC9sb2dpbiIsImlhdCI6MTcwNDIwNTI1MSwiZXhwIjoxNzA0MzQ5MjUxLCJuYmYiOjE3MDQyMDUyNTEsImp0aSI6IlVsUmgwaXp3QTJVVG1QQzciLCJzdWIiOiIyNzMiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.gncykAqwnvvy_lIqeRleTXk1llwQwsX48wFny6FGGpM&prenom=&name=&email=&tel=&actif=0&adresse=&civilite=&matriculeFiscale=&governorats=&delegation=&created_at=&id=0&nbr=25&page=1'
+SECURE_URL='https://core.jax-delivery.com/api/clients?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NvcmUuamF4LWRlbGl2ZXJ5LmNvbS9hcGkvYXV0aC9sb2dpbiIsImlhdCI6MTcwNDIzMDIzMSwiZXhwIjoxNzA0Mzc0MjMxLCJuYmYiOjE3MDQyMzAyMzEsImp0aSI6InVyemcwNjRleFl2M1RQSTQiLCJzdWIiOiIyNzMiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.BuzcjXe6C_4CrB40ukKZ2sfdOffSbYyQzWwB4c_0phE&prenom=&name=&email=&tel=&actif=0&adresse=&civilite=&matriculeFiscale=&governorats=&delegation=&created_at=&id=0&nbr=25&page='
  
 URL = 'https://admin.jax-delivery.com/#'
 LOGIN_ROUTE=('https://core.jax-delivery.com/api/auth/login')
@@ -27,18 +27,26 @@ login_payload={'email': "mohameddhiabensaad@gmail.com",' password': "192587"}
 
 with requests.session() as s :
     s.post(LOGIN_ROUTE,data=login_payload)
-    r=s.get(SECURE_URL)
-    # soup=BeautifulSoup(r.text)
-    print(r.text)
-    data = json.loads(r.text)["data"]
-
-    with open("output.csv", "w", newline="", encoding="utf-8") as csv_file:
-        csv_writer = csv.writer(csv_file)
+    page = 1
+    while True:
+        r = s.get(SECURE_URL + str(page))
+        data = json.loads(r.text)["data"]
         
-        # Write header
-        header = data[0].keys()
-        csv_writer.writerow(header)
+        if not data:
+            break  # No more data, exit the loop
 
-        # Write data
-        for entry in data:
-            csv_writer.writerow(entry.values())
+        header = list(data[0].keys())
+        index = [header.index(i) for i in ['code', 'prenom', 'matriculeFiscale', 'frais_liv', 'frais_retour']]
+        
+
+        with open("output.csv", "a", newline="", encoding="utf-8") as csv_file:  # Use "a" to append to the file
+            Lheader = ['code', 'prenom', 'matriculeFiscale', 'frais_liv', 'frais_retour']
+            csv_writer = csv.DictWriter(csv_file, fieldnames=Lheader)
+            if page ==1:
+                csv_writer.writeheader()  # Write header only for the first page
+            for entry in data:
+                l = list(entry.values())
+                csv_writer.writerow({'code': l[index[0]], 'prenom': l[index[1]], 'matriculeFiscale': l[index[2]],
+                                     'frais_liv': l[index[3]], 'frais_retour': l[index[4]]})
+        page += 1
+        
---
+++ scrap2.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import csv
+import json
+import requests
+
+
+
+
+SECURE_URL='https://core.jax-delivery.com/api/colis?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NvcmUuamF4LWRlbGl2ZXJ5LmNvbS9hcGkvYXV0aC9sb2dpbiIsImlhdCI6MTcwNDIzMDIzMSwiZXhwIjoxNzA0Mzc0MjMxLCJuYmYiOjE3MDQyMzAyMzEsImp0aSI6InVyemcwNjRleFl2M1RQSTQiLCJzdWIiOiIyNzMiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.BuzcjXe6C_4CrB40ukKZ2sfdOffSbYyQzWwB4c_0phE&adresseLivraison=&cod=&code=&created_at=&depot=&is_echange=nonechange&description=&governorat=&nomContact=&referenceExterne=&statut_id=&tel=&paye=&client_id=&livre_par=&verse=&date_debut_enlev=&date_fin_enlev=&date_debut_livraison=&date_fin_livraison=&date_debut_retour=&date_fin_retour=&id=0&nbr=25&page='
+ 
+URL = 'https://admin.jax-delivery.com/#'
+LOGIN_ROUTE=('https://core.jax-delivery.com/api/auth/login')
+
+HEADERS={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36','origin':URL,'Referer':URL+LOGIN_ROUTE}
+
+
+
+
+login_payload={'email': "mohameddhiabensaad@gmail.com",' password': "192587"}
+
+with requests.session() as s :
+        s.post(LOGIN_ROUTE,data=login_payload)
+        page = 1
+        en_attente_count = 0 
+        expi =0
+        while True :
+                r = s.get(SECURE_URL + str(page))
+                data = json.loads(r.text)["data"]
+                
+                if not data:
+                    break  # No more data, exit the loop
+
+                header = list(data[0].keys())
+                index = [header.index(i) for i in ['code','status']]
+                # print(index)
+
+                with open("output3.csv", "a", newline="", encoding="utf-8") as csv_file:  # Use "a" to append to the file
+                    Lheader = ['code','status']
+                    csv_writer = csv.DictWriter(csv_file, fieldnames=Lheader)
+                    if page ==1:
+                        csv_writer.writeheader()  # Write header only for the first page
+                    for entry in data:
+                            l = list(entry.values())
+                            status_value = l[index[1]][0]['libelle'] if l[index[1]] else ''  # Extract 'libelle' value from the list
+                            csv_writer.writerow({'code': l[index[0]], 'status': status_value})
+                             # Check if 'libelle' is "en attente" and increment the count
+                            if status_value == 'En attente':
+                                en_attente_count += 1
+                            elif status_value =='Reçu par l`expediteur':
+                                  expi+=1
+                           
+                
+                with open("output3.text", "w", newline="", encoding="utf-8") as file:
+                       file.write('Total en attent:'+str(en_attente_count)+"\n")
+                       file.write('Total en expi:'+str(expi))
+                page += 1
+        
+print(expi,en_attente_count)
